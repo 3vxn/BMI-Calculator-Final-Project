03bd5c9c799c6a011a3499860ca61a25bec0f62b
3vxn <evan72003@me.com>
2022-04-04 12:52:36 -0400

    Initial Commit

---
+++ BMI Calculator Final Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculator Final Project
+//
+//  Created by Evan Hartnett on 4/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
